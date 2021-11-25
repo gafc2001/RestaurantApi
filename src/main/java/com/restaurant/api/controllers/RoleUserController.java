0d4f355f5dc9b9c0fc77faccd8f561f54b41739f
Gustavo Farfan <farfancoraje15@gmail.com>
2021-11-24 20:46:03 -0500

    Role request

--- src/main/java/com/restaurant/api/controllers/RoleUserController.java
+++ src/main/java/com/restaurant/api/controllers/RoleUserController.java
@@ -1,15 +1,18 @@
 package com.restaurant.api.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
+import com.restaurant.api.dto.response.RoleRequest;
+import com.restaurant.api.models.User;
+import com.restaurant.api.repositories.RoleRepository;
+import com.restaurant.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.restaurant.api.models.RoleUser;
@@ -22,9 +25,16 @@ import com.restaurant.api.service.RoleUserService;
 public class RoleUserController {
 
 	@Autowired 
-	RoleUserService _rolesUserService;
-	
-	@RequestMapping(value = "/",method = RequestMethod.GET)
+	private RoleUserService _rolesUserService;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+
+	@GetMapping("/")
 	public ResponseEntity<?> getRoles(){
 		List<RoleUser> roles = _rolesUserService.findAllRoles();
 		if(roles.isEmpty() || roles == null) {
@@ -32,7 +42,7 @@ public class RoleUserController {
 		}
 		return new ResponseEntity<List<RoleUser>>(roles,HttpStatus.OK);
 	}
-	@RequestMapping(value = "/", method = RequestMethod.POST ,headers = "Accept=application/json")
+	@PostMapping("/")
 	public ResponseEntity<?> saveRoles(@RequestBody RoleUser roleUser,UriComponentsBuilder uriComponentsBuilder){
 		if(roleUser == null) {
 			return new ResponseEntity<>(new MessageResponse("Provide a role"), HttpStatus.NO_CONTENT);
@@ -43,5 +53,37 @@ public class RoleUserController {
 		_rolesUserService.saveRoleUser(roleUser);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
+	@PostMapping("/users/{id}")
+	public ResponseEntity<?> updateRoles(@PathVariable("id")Long id, @RequestBody RoleRequest roleRequest){
+		Optional<User> userOpt =userRepository.findById(id);
+		if(!userOpt.isPresent()){
+			return ResponseEntity.badRequest().body(new MessageResponse("User not FOUND"));
+		}
+		User user = userOpt.get();
+		Set<RoleUser> roles = user.getRoles();
+		roles.clear();
+		switch (roleRequest.getRole()) {
+			case "admin":
+				RoleUser adminRole = roleRepository.findByNameRole("ROLE_ADMIN")
+						.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+				roles.add(adminRole);
+
+				break;
+			case "mod":
+				RoleUser modRole = roleRepository.findByNameRole("ROLE_MODERATOR")
+						.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+				roles.add(modRole);
+
+				break;
+			default:
+				RoleUser userRole = roleRepository.findByNameRole("ROLE_USER")
+						.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+				roles.add(userRole);
+		}
+		user.setRoles(roles);
+		User userSaved = userRepository.save(user);
+		return ResponseEntity.ok().body(userSaved);
+	}
+
 	
 }
